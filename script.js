99c81d3f984f2bd81e1033691b37c838fae1fd25
dbakinovskaya <d.bakinovskaya@gmail.com>
2023-05-03 18:47:30 +0400

    feat: added common methods

--- script.js
+++ script.js
@@ -3,16 +3,35 @@ class BaseClass {
         this.value = value;
     }
 
+    get() {
+        return this.value
+    }
+
+    plus(...values) {
+        this.value = values.reduce((a,b) => a+b, this.value);
+        return this;
+    }
 }
+
 //ES6
 class IntBuilder extends BaseClass {
+    constructor(value) {
+        super(value);
+        if (typeof(this.value) != 'number') {
+            this.value = 0;
+        }
+    }
+
 
 }
 
 //ES5
 function StringBuilder(value) {
-    Object.assign(this, new BaseClass(value))
-
+    Object.assign(this, new BaseClass(value));
+    if (typeof(this.value) != 'string') {
+        this.value = '';
+    }
 }
+
 StringBuilder.prototype = Object.create(BaseClass.prototype);
 
